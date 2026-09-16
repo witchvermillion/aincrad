02b01694cd74d7e7825657f10d7e6db8153faecc
cutiepiedev <pro.issei@gmail.com>
2026-09-15 23:34:07 -0400

    chore: remove Avaje Inject module annotation from redisson module

--- cardinal/integration/redisson/src/main/java/module-info.java
+++ cardinal/integration/redisson/src/main/java/module-info.java
@@ -1,6 +1,3 @@
-import io.avaje.inject.InjectModule;
-
-@InjectModule(name = "redisson")
 module dev.witchvermillion.cardinal.redisson {
   requires static org.jspecify;
   requires io.avaje.inject;
