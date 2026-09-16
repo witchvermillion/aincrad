7b6c646b9aa86cb1be369e0696f2b23998623672
cutiepiedev <pro.issei@gmail.com>
2026-09-15 23:33:02 -0400

    chore: remove unused Avaje Inject dependency from profile module

--- cardinal/feature/profile/api/src/main/java/module-info.java
+++ cardinal/feature/profile/api/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module dev.witchvermillion.cardinal.profile {
   requires static org.jetbrains.annotations;
   requires static org.jspecify;
-  requires io.avaje.inject;
   requires reactor.core;
 
   exports dev.witchvermillion.cardinal.profile;
