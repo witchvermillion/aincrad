3a55122c41921d8128929aef0874e5a02c3cc58d
cutiepiedev <pro.issei@gmail.com>
2026-09-16 15:51:38 -0400

    refactor: remove bulk profile operations from ProfileRegistrar and ProfileLoader

--- cardinal/feature/profile/api/src/main/java/dev/witchvermillion/cardinal/profile/ProfileRegistrar.java
+++ cardinal/feature/profile/api/src/main/java/dev/witchvermillion/cardinal/profile/ProfileRegistrar.java
@@ -2,13 +2,10 @@ package dev.witchvermillion.cardinal.profile;
 
 import java.util.UUID;
 import org.jetbrains.annotations.ApiStatus;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @ApiStatus.NonExtendable
 public interface ProfileRegistrar {
 
   Mono<Profile> registerProfile(final UUID profileId);
-
-  Flux<Profile> registerProfiles(final Iterable<UUID> profileIds);
 }
