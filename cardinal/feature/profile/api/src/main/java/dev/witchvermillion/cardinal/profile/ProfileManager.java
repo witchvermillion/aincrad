ab503428db06a3bfd5b1f920152e9c8d0f063d09
cutiepiedev <pro.issei@gmail.com>
2026-09-16 16:17:03 -0400

    feat: extend ProfileManager to include ProfileRegistry interface

--- cardinal/feature/profile/api/src/main/java/dev/witchvermillion/cardinal/profile/ProfileManager.java
+++ cardinal/feature/profile/api/src/main/java/dev/witchvermillion/cardinal/profile/ProfileManager.java
@@ -3,4 +3,4 @@ package dev.witchvermillion.cardinal.profile;
 import org.jetbrains.annotations.ApiStatus;
 
 @ApiStatus.NonExtendable
-public interface ProfileManager extends ProfileRegistrar, ProfileLoader {}
+public interface ProfileManager extends ProfileRegistrar, ProfileLoader, ProfileRegistry {}
