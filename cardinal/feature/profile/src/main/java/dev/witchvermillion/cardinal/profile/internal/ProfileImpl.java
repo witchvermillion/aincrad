0b4921166cb81274aaf0384b78e31484ae05c6b2
cutiepiedev <pro.issei@gmail.com>
2026-09-15 22:55:48 -0400

    feat: implement Profile interface in ProfileImpl

--- cardinal/feature/profile/src/main/java/dev/witchvermillion/cardinal/profile/internal/ProfileImpl.java
+++ cardinal/feature/profile/src/main/java/dev/witchvermillion/cardinal/profile/internal/ProfileImpl.java
@@ -1,7 +1,8 @@
 package dev.witchvermillion.cardinal.profile.internal;
 
+import dev.witchvermillion.cardinal.profile.Profile;
 import java.time.Instant;
 import java.util.UUID;
 import org.bson.codecs.pojo.annotations.BsonId;
 
-record ProfileImpl(@BsonId UUID id, Instant createdAt) {}
+record ProfileImpl(@BsonId UUID id, Instant createdAt) implements Profile {}
