b2c695e8fae81684a17858447e59c6cf20aaea2c
cutiepiedev <pro.issei@gmail.com>
2026-09-17 13:12:52 -0400

    refactor: remove `avaje-inject` dependency from profile-api module

--- cardinal/feature/profile/api/src/main/java/module-info.java
+++ cardinal/feature/profile/api/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module dev.witchvermillion.cardinal.profile.api {
   requires static org.jetbrains.annotations;
   requires static org.jspecify;
   requires reactor.core;
-  requires io.avaje.inject;
 
   exports dev.witchvermillion.cardinal.profile;
 }
