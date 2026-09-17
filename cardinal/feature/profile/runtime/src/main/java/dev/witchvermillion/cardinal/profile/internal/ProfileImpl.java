106cc7b406e1eb0771fa65e2316e835415f874ea
cutiepiedev <pro.issei@gmail.com>
2026-09-17 13:06:36 -0400

    refactor: remove @BsonId annotation from ProfileImpl definition

--- cardinal/feature/profile/runtime/src/main/java/dev/witchvermillion/cardinal/profile/internal/ProfileImpl.java
+++ cardinal/feature/profile/runtime/src/main/java/dev/witchvermillion/cardinal/profile/internal/ProfileImpl.java
@@ -3,6 +3,5 @@ package dev.witchvermillion.cardinal.profile.internal;
 import dev.witchvermillion.cardinal.profile.Profile;
 import java.time.Instant;
 import java.util.UUID;
-import org.bson.codecs.pojo.annotations.BsonId;
 
-record ProfileImpl(@BsonId UUID id, Instant createdAt) implements Profile {}
+record ProfileImpl(UUID id, Instant createdAt) implements Profile {}
