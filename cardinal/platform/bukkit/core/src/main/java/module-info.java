b794ad369ad5b6c870e299a117390abf6beda304
cutiepiedev <pro.issei@gmail.com>
2026-09-18 15:08:42 -0400

    refactor: remove unused `exports` statement in module-info for cleaner module definition

--- cardinal/platform/bukkit/core/src/main/java/module-info.java
+++ cardinal/platform/bukkit/core/src/main/java/module-info.java
@@ -2,6 +2,4 @@ module dev.witchvermillion.cardinal.bukkit.core {
   requires io.avaje.inject;
   requires jakarta.inject;
   requires spigot.api;
-
-  exports dev.witchvermillion.cardinal.bukkit.plugin;
 }
