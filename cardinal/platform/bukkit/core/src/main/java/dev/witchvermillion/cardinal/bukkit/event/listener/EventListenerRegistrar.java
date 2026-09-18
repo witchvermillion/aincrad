fe2da291ad6470c6c41f4ec5cf4721aa47c97291
cutiepiedev <pro.issei@gmail.com>
2026-09-18 02:05:44 -0400

    feat: add `cardinal-platform` module with Bukkit support, dependencies, and listener registration

---
+++ cardinal/platform/bukkit/core/src/main/java/dev/witchvermillion/cardinal/bukkit/event/listener/EventListenerRegistrar.java
@@ -0,0 +1,28 @@
+package dev.witchvermillion.cardinal.bukkit.event.listener;
+
+import dev.witchvermillion.cardinal.bukkit.plugin.BukkitPlugin;
+import io.avaje.inject.PostConstruct;
+import jakarta.inject.Singleton;
+import java.util.Set;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+
+@Singleton
+final class EventListenerRegistrar {
+
+  private final Plugin plugin;
+  private final Set<Listener> listeners;
+
+  EventListenerRegistrar(final @BukkitPlugin Plugin plugin, final Set<Listener> listeners) {
+    this.plugin = plugin;
+    this.listeners = listeners;
+  }
+
+  @PostConstruct
+  void registerListeners() {
+    for (final Listener listener : this.listeners) {
+      Bukkit.getPluginManager().registerEvents(listener, this.plugin);
+    }
+  }
+}
---
+++ cardinal/platform/bukkit/core/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module dev.witchvermillion.cardinal.bukkit.core {
+  requires io.avaje.inject;
+  requires jakarta.inject;
+  requires spigot.api;
+
+  exports dev.witchvermillion.cardinal.bukkit.plugin;
+}
