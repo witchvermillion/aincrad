5e87a4bd03c11cd886c7081cd585343b7dcc5f0c
cutiepiedev <pro.issei@gmail.com>
2026-09-21 03:03:06 -0400

    refactor: remove `EntityIndex` and its usages; simplify participant entity management in `ParticipantEntityManagerImpl` with `ParticipantEntityIndex`

--- seed/feature/participant/runtime/src/main/java/dev/witchvermillion/seed/participant/internal/ParticipantEntityIndex.java
+++ seed/feature/participant/runtime/src/main/java/dev/witchvermillion/seed/participant/internal/ParticipantEntityIndex.java
@@ -1,17 +1,6 @@
 package dev.witchvermillion.seed.participant.internal;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
+import jakarta.inject.Singleton;
 
-import jakarta.inject.Qualifier;
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Qualifier
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({FIELD, PARAMETER, METHOD})
-@interface ParticipantEntityIndex {}
+@Singleton
+final class ParticipantEntityIndex {}
--- seed/feature/participant/runtime/src/main/java/dev/witchvermillion/seed/participant/internal/ParticipantEntityManagerImpl.java
+++ seed/feature/participant/runtime/src/main/java/dev/witchvermillion/seed/participant/internal/ParticipantEntityManagerImpl.java
@@ -1,7 +1,5 @@
 package dev.witchvermillion.seed.participant.internal;
 
-import dev.witchvermillion.seed.entity.index.EntityIndex;
-import dev.witchvermillion.seed.participant.Participant;
 import dev.witchvermillion.seed.participant.ParticipantEntityManager;
 import dev.witchvermillion.seed.participant.ParticipantEntityRegistrar;
 import dev.witchvermillion.seed.participant.ParticipantEntityRegistry;
@@ -22,51 +20,28 @@ import org.jspecify.annotations.Nullable;
 final class ParticipantEntityManagerImpl implements ParticipantEntityManager {
 
   private final World world;
-  private final EntityIndex<UUID> participantEntityIndex;
+  private final ParticipantEntityIndex participantEntityIndex;
 
   ParticipantEntityManagerImpl(
-      final World world, final @ParticipantEntityIndex EntityIndex<UUID> participantEntityIndex) {
+      final World world, final ParticipantEntityIndex participantEntityIndex) {
     this.world = world;
     this.participantEntityIndex = participantEntityIndex;
   }
 
   @Override
   public void registerParticipantEntity(
-      final UUID participantId, final String participantUsername) {
-    this.participantEntityIndex
-        .entity(participantId)
-        .ifPresentOrElse(
-            participantEntity -> {
-              if (!participantUsername.equals(participantEntity.name())) {
-                participantEntity.name(participantUsername);
-              }
-            },
-            () -> {
-              final Entity participantEntity =
-                  this.world
-                      .obtainEntity(this.world.entity())
-                      .name(participantUsername)
-                      .add(Participant.class);
-
-              this.participantEntityIndex.putEntity(participantId, participantEntity);
-            });
-  }
+      final UUID participantId, final String participantUsername) {}
 
   @Override
-  public void unregisterParticipantEntity(final UUID participantId) {
-    final Entity participantEntity = this.participantEntityIndex.removeEntityByKey(participantId);
-    if (participantEntity != null) {
-      participantEntity.destruct();
-    }
-  }
+  public void unregisterParticipantEntity(final UUID participantId) {}
 
   @Override
   public @Nullable Entity participantEntityOrNull(final UUID participantId) {
-    return this.participantEntityIndex.entityOrNull(participantId);
+    return null;
   }
 
   @Override
   public Optional<Entity> participantEntity(final UUID participantId) {
-    return this.participantEntityIndex.entity(participantId);
+    return Optional.ofNullable(this.participantEntityOrNull(participantId));
   }
 }
