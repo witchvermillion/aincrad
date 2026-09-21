320a6c28a2a38ae92e8710c7e3e9f40c2bb86dc7
cutiepiedev <pro.issei@gmail.com>
2026-09-21 14:37:29 -0400

    refactor: simplify participant entity creation logic in `ParticipantEntityManagerImpl` by replacing `putIfAbsent` with explicit null check

--- seed/feature/participant/runtime/src/main/java/dev/witchvermillion/seed/participant/internal/ParticipantEntityManagerImpl.java
+++ seed/feature/participant/runtime/src/main/java/dev/witchvermillion/seed/participant/internal/ParticipantEntityManagerImpl.java
@@ -42,10 +42,9 @@ final class ParticipantEntityManagerImpl implements ParticipantEntityManager {
     requireNonNull(participantId, PARTICIPANT_ID_CANNOT_BE_NULL);
     requireNonNull(participantUsername, "Participant username cannot be null");
 
-    final Entity participantEntity =
-        this.participants.putIfAbsent(
-            participantId, this.createParticipantEntity(participantUsername));
+    final Entity participantEntity = this.participants.get(participantId);
     if (participantEntity == null) {
+      this.participants.put(participantId, this.createParticipantEntity(participantUsername));
       return;
     }
 
