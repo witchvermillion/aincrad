ceff3c4a453e370642c1a5d575171db6d7c25c42
cutiepiedev <pro.issei@gmail.com>
2026-09-24 16:17:46 -0400

    refactor: remove `AbilityDefinition`, `AbilityDefinitionManager`, and related implementations from `ability` feature

--- seed/feature/ability/api/src/main/java/dev/witchvermillion/seed/ability/AbilityDefinitionManager.java
+++
@@ -1,6 +0,0 @@
-package dev.witchvermillion.seed.ability;
-
-import org.jetbrains.annotations.ApiStatus;
-
-@ApiStatus.NonExtendable
-public interface AbilityDefinitionManager extends AbilityDefinitionRegistry {}
--- seed/feature/ability/api/src/main/java/dev/witchvermillion/seed/ability/package-info.java
+++
@@ -1,4 +0,0 @@
-@NullMarked
-package dev.witchvermillion.seed.ability;
-
-import org.jspecify.annotations.NullMarked;
